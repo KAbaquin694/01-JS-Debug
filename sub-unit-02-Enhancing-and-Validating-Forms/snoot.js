fffa5ab8c8851544a9d65a0265518783a2296b2e
Khalel <KAbaquin@west-mec.org>
2019-08-16 15:25:56 -0700

    Final Update (Unfinished)
    fixed a 'bug' that makes no sense but somehow still works? (nevermind) added more if & else/if statements, added some functions, there's some broken code that I do not understand r.i.p.

--- sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
+++ sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
@@ -6,7 +6,7 @@ var thirty = document.createDocumentFragment();
 var thirtyOne = document.createDocumentFragment();
 
 function setUpDays() {
-    var dates = document.getElemntsById("delivDy");
+    var dates = document.getElementsById("delivDy");
     twentyNine.appendChild(dates[28].cloneNode(true))
     thirty.appendChild(dates[28].cloneNode(true));
     thirty.appendChild(dates[29].cloneNode(true));
@@ -25,7 +25,13 @@ function updateDays() {
         deliveryDay.removeChild(dates[28]);
     } if (deliveryYear.selectedIndex === -1) {
         deliveryYear.selectedIndex = 0;
-    } if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex]) 
+    } if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    } else if (selecetedMonth === "4" || selectedMonth === "6" || slectedMonth === "9" || selectedMonth === "11") {
+        deliveryDay.appendchild(thirty.cloneNode(true));
+    } else if (selecetedMonth === "1" || selectedMonth === "3" || slectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+        deliveryDay.appendchild(thirtyOne.cloneNode(true));
+    }
 }
 
 // run setUpPage function when page finishes loading
@@ -42,7 +48,42 @@ function removeSelectDefaults() {
 
 function setUpPage() {
     removeSelectDefaults();
+    setUpDays();
+    createEventListener();
 }
+
+function createEventListeners() {
+    var deliveryMonth = document.getElementById("delivMo");
+    if (deliveryMonth.addEventListener){
+        deliveryMonth.addEventListener("change", updateDays, false);
+    } else if (deliveryMonth.attachEvent) {
+        deliveryMonth.attachEvent("onchange", updateDays);
+    }
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryYear.addEventListener) {
+        deliveryYear.addEventListener("change", updateDays, false);
+    } else if (deliveryYear.attachEvent) {
+        deliveryYear.attachEvent("onchange", updateDays);
+    }
+}
+
+function autoCheckCustom() {
+    var messageBox = document.getElementById("customText");
+    if (messageBox.value !== "" &&  messageBox.value !== messageBox.placeholder) { document.getElementById("custom").checked ="checked"; 
+} else {
+    document.getElementById("custom").checked = "";
+}
+}
+
+function copyBillingAddress() {
+    var billingInputElements = document.querySelectorAll("#billingAddress input");
+    var deliveryInputElements = document.querySelectorAll("#deliveryAddress input");
+    if (document.getElementById("sameAddr").checked) {
+        for (var i = 0; i < billingInputElements.length; i++) {
+            deliveryInputElements[i +1].value = billingInputElements[i].value;
+        } 
+        document.querySelector("#deliveryAddress select").value = document.querySelector("#billingAddress select").value;
+}}
 
 function createEventListener() {
     var sumbitButton = document.getElementById("submit");
@@ -53,4 +94,20 @@ function createEventListener() {
     } for (var i = 0; i < emptyBoxes.length; i++) {
         emptyBoxes[i].selectedIndex = -1;
     }
+    var same = document.getElementById("sameAddr");
+    if (same.addEventListener) {
+        same.addEventListener("change", copyBillingAddress,false);
+    } else if (same.attachEvent) {
+        same.attachEvent("onchange", copyBillingAddress);
+    }
 }
+
+
+    function autoCheckFunction() {
+        var messageBox = document.getElementById("customText");
+        if (messageBox.addEventListener) {
+            messageBox.addEventListener("change", autoCheckCustom, false);
+        }  else if (messageBox.attachEvent) {
+            messageBox.attachEvent("onchange", autoCheckCustom)
+        }
+    }
